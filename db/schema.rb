914662eec425c1ff2d7f0d83c3a8b18cd41ecd63
Ruy Rocha <admin@ruyrocha.com>
2018-09-29 06:37:39 -0300

    Create User model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_29_071111) do
+ActiveRecord::Schema.define(version: 2018_09_29_093615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 2018_09_29_071111) do
     t.datetime "updated_at", null: false
     t.index ["movie_id"], name: "index_roles_on_movie_id"
     t.index ["person_id"], name: "index_roles_on_person_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "aliases", "people"
--- spec/models/producer_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Producer, type: :model do
-  it_behaves_like "an STI class"
-end

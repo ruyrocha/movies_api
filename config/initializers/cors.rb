71e29550d4c73fda0b233abff20b4811c609aae1
Ruy Rocha <admin@ruyrocha.com>
2018-09-29 12:40:30 -0300

    Fix CORS and start-up script.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'localhost:8080', 'localhost:4040'
+    origins 'localhost:4040', 'localhost:3000'
 
     resource '*',
       headers: :any,
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Dropping databases..."
-bundle exec rake db:drop
-echo "Creating databases..."
-bundle exec rake db:create
-echo "Migrating..."
-bundle exec rake db:migrate
-echo "Seeding..."
-bundle exec rake db:seed
+echo "Re-create databases and add seeds..."
+bundle exec rails db:setup
 echo "Removing stale PID file"
 /bin/rm tmp/pids/server.pid
 echo "Starting the Rails server..."
